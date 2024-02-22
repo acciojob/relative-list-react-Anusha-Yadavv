992192676cdf02573f55644670181aa5cdabf246
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-02-22 13:42:14 +0530

    making changes

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,10 @@ const App = () => {
       <ol key="relativeList">
         {
           relatives.map((relative,index)=>{
+            return(
 
             <li key={`relativeListItem+${index+1}`}>{relative}</li>
+            )
 
           })
         }
